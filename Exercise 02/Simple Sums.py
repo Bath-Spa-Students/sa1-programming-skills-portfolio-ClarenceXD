1ba99413e77893bd7d325bf2ea8edd06229185fd
ClarenceXD <dsouza.clarence2007@gmail.com>
2024-10-15 13:08:06 +0400

    Simple Sums

---
+++ Exercise 02/Simple Sums.py
@@ -0,0 +1,5 @@
+firstnumber = 8                                                        #variable first number is a int containing a whole number 
+secondnumber = 10
+results = firstnumber + secondnumber                                   #variable results is used to print the results of adding first and second number 
+
+print (results)                                                        #prints the results that is the total of the first number and the second number
