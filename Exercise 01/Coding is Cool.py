6972a59162c6dd4fde4f49c99c61aee01f026acc
ClarenceXD <dsouza.clarence2007@gmail.com>
2024-10-15 13:07:55 +0400

    Coding is Cool

---
+++ Exercise 01/Coding is Cool.py
@@ -0,0 +1,8 @@
+word1 = "Coding "                            #word1 is the variable containing the string value "Coding "
+word2 = "is "                                #word2 is the variable containing the string value "is "
+word3 = "Cool"                               #word3 is the variable containing the string value "Cool"
+
+print (word1 + word2 + word3)
+
+"""String concatenation is used to add strings together, 
+using the character "+" placed inside the print statement between 2 variables will combine them."""
