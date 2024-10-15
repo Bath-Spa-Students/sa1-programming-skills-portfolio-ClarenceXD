07e68b7f5f86b7b46b9a27f07a7c59d47487e1ed
ClarenceXD <dsouza.clarence2007@gmail.com>
2024-10-15 13:23:00 +0400

    Coding is Cool, Simple Sums, Biography

--- Exercise 01/Coding is Cool.py
+++ Exercise 01/Coding is Cool.py
@@ -1,3 +1,18 @@
+
+"""Fill in the blanks in the Python code below to output the phrase **"Coding is Cool"** to the console using variables and string concatenation.
+
+
+### Fill in the blanks below
+```python
+word1 = 
+word2 = 
+word3 =
+
+# Use string concatenation to combine the variables and print the phrase
+_____(_________)"""
+
+
+
 word1 = "Coding "                            #word1 is the variable containing the string value "Coding "
 word2 = "is "                                #word2 is the variable containing the string value "is "
 word3 = "Cool"                               #word3 is the variable containing the string value "Cool"
--- Exercise 02/Simple Sums.py
+++ Exercise 02/Simple Sums.py
@@ -1,3 +1,11 @@
+"""In this exercise, you will create and work with integer variables, perform arithmetic operations, and print the result to the console.
+
+### Steps:
+1. Declare a variable and initialize it with the integer value `8`.
+2. Declare a second variable and initialize it with the integer value `10`.
+3. Declare a third variable that stores the sum of first two numbers.
+4. Print the value of the sum to the console."""
+
 firstnumber = 8                                                        #variable first number is a int containing a whole number 
 secondnumber = 10
 results = firstnumber + secondnumber                                   #variable results is used to print the results of adding first and second number 
