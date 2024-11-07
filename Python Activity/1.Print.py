f640854f402436f56da3a726bce3fd33bfa66b78
ClarenceXD <dsouza.clarence2007@gmail.com>
2024-11-08 02:28:50 +0400

    Print

---
+++ Python Activity/1.Print.py
@@ -0,0 +1,21 @@
+name = "Clarence D'Souza"
+
+print (f""" Who i am:
+    -I am Clarence D'Souza. I am currently learning Python using visual studio code in the Bath Spa University RAK.
+    -I live around 15-30 mins away from the University using car.
+    -I pick the course know as Creative Computing as it felt very intersting to me.
+       
+    {name} 
+    Thanks """)
+
+
+"""Single and double quotation marks can be used for printing multi line outputs"""
+print("----------------------------------------------------------------------------------------------------------------")
+
+print ("""In order to write comments that will not be displayed in the output type '#' and the text following after it
+    this text should be on another line 
+        and this should be on the 3rd line
+            this makes it like a stair case
+                going to the right 
+                    slowly
+                        and slowly""")     #comment (❤️´艸｀❤️) 
