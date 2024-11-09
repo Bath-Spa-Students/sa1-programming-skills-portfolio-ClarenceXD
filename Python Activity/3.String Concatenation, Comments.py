21662bff6622c2931ba99613ec1497cb10601168
ClarenceXD <dsouza.clarence2007@gmail.com>
2024-11-09 22:03:56 +0400

    String Concatenation, Comments

---
+++ Python Activity/3.String Concatenation, Comments.py
@@ -0,0 +1,10 @@
+"""String Concatenation is used to combind string.
+Single and double quotation marks can be used for multi line comments that do not affect the code"""
+
+#Hashtag is used to created 1 line comments
+
+first_word = "Hello "
+second_word = "my name is "
+third_word = "Clarence"
+
+print (first_word + second_word + third_word)
