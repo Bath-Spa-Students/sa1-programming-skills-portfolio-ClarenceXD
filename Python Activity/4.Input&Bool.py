562ded076ab7b12e719b58be31f4eea8e2e427e4
ClarenceXD <dsouza.clarence2007@gmail.com>
2024-11-09 22:04:19 +0400

    Input

---
+++ Python Activity/4.Input&Bool.py
@@ -0,0 +1,7 @@
+word = input("Enter your Name: ")
+print (word)
+
+age = input("How old are you: ")
+print(age)
+word = bool(word)
+print (f"If true the age has a string or input if not false: {word}")          #converting string to bool can check whether the user typed anything or not
