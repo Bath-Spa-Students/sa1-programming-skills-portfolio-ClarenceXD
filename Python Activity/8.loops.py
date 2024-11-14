26cbc163259c0b5a7b201c903d69aea956a48449
ClarenceXD <dsouza.clarence2007@gmail.com>
2024-11-15 02:03:40 +0400

    loops

---
+++ Python Activity/8.loops.py
@@ -0,0 +1,11 @@
+for first in range(15,150,5):                             #start from 15 till 150 interval of 5 last data is not in output
+    print (first)
+print("Out of For Loop")
+
+number = 1
+
+while True:
+    print(f"This number will increase by 1 every loop {number}")
+    number +=1
+    if number > 4000:
+        break
