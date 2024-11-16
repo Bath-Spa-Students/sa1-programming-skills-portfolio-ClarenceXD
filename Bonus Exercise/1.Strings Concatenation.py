889c17b6439cf8accbaa7169bd19a1556af3cc6f
ClarenceXD <dsouza.clarence2007@gmail.com>
2024-11-16 18:49:47 +0400

    Strings Concatenation

---
+++ Bonus Exercise/1.Strings Concatenation.py
@@ -0,0 +1,7 @@
+first_string = "Hello "
+second_string = "my name is "
+third_string = "Clarence"
+#This uses String concatenation to combine 3 different string values into 1
+Total_string = first_string + second_string + third_string      
+
+print(Total_string) 
