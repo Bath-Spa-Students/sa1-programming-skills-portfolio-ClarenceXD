d816ef1f9164eebcb64b500038a0a1b8682d9833
ClarenceXD <dsouza.clarence2007@gmail.com>
2024-11-20 16:22:12 +0400

    List - Names

---
+++ Bonus Exercise/4.List - Names.py
@@ -0,0 +1,10 @@
+"""Store the names of a few of your friends in a list called names. Print each person’s name
+by accessing each element in the list, one at a time."""
+
+my_friends = ["Jacob","John","Adam","Myself :D"]
+
+print("my friends are") 
+print(my_friends[0]) 
+print(my_friends[1]) 
+print(my_friends[2]) 
+print(my_friends[3])                                                              #print(*my_friends, sep='\n')
