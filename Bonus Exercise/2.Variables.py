32b00dd1c4a5495e393b673ee0fcf7e2c0610a8d
ClarenceXD <dsouza.clarence2007@gmail.com>
2024-11-21 12:01:53 +0400

    Strings Concatenation

--- Bonus Exercise/2.Variables.py
+++
@@ -1,4 +0,0 @@
-message = "My name is Clarence. It is nice to meet you"
-print(message)
-message = "How are you today?"
-print(message)
--- Bonus Exercise/6.Dictionaries.py
+++
@@ -1,6 +0,0 @@
-user_info_name = ("Erlinda Fajardo")
-names = user_info_name.split()                           #code was first made to grab input from user
-user_age = ("70")
-
-Dictionarie = {"first_name":names[-2], "last_name": names[-1],"age": user_age}     
-print(*Dictionarie.values(), sep = '\n')
